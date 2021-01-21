07f2d6da891cbd3b2ff15a0b0271eb839f08662f
Malkon Faria <sudo777@protonmail.com>
2021-01-21 19:58:53 -0300

    Implementando listagem de tarefas

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TarefaService } from './shared';
 import { ListarTarefaComponent } from './listar';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ListarTarefaComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   providers: [TarefaService],
 })
 export class TarefasModule {}
