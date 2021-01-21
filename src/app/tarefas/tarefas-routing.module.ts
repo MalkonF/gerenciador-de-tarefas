67971f8980c617df2dbdddcef33f7ffb9a52dae4
Malkon Faria <sudo777@protonmail.com>
2021-01-21 20:15:57 -0300

    Definindo rota para cadastro tarefas e criando html do cadastro de tarefas

--- src/app/tarefas/tarefas-routing.module.ts
+++ src/app/tarefas/tarefas-routing.module.ts
@@ -1,16 +1,20 @@
 import { Routes } from '@angular/router';
+import { CadastrarTarefaComponent } from './cadastrar';
 
 import { ListarTarefaComponent } from './listar';
 
-
 export const TarefaRoutes: Routes = [
-	{ 
-		path: 'tarefas', 
-		redirectTo: 'tarefas/listar' 
-	},
-	{ 
-		path: 'tarefas/listar', 
-		component: ListarTarefaComponent 
-	}, 
-	
+  {
+    path: 'tarefas',
+    redirectTo: 'tarefas/listar',
+  },
+  {
+    path: 'tarefas/listar',
+    component: ListarTarefaComponent,
+  },
+
+  {
+    path: 'tarefas/cadastrar',
+    component: CadastrarTarefaComponent,
+  },
 ];
